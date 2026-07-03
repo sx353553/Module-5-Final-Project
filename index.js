3e24b618768d3ecee5aae903ed929e07b1423a73
sx353553 <sx353553@yahoo.com>
2026-07-02 19:24:25 -0500

    update code

---
+++ index.js
@@ -0,0 +1,44 @@
+const sortFilter = document.querySelector("#sort-filter");
+const locationList = document.querySelector("#location-list");
+
+const originalItems = Array.from(locationList.querySelectorAll("li"));
+
+sortFilter.addEventListener("change", () => {
+  let items = [...originalItems];
+
+  if (sortFilter.value === "city-az") {
+    items.sort((a, b) => {
+      const cityA = a.textContent.split(",")[0].trim();
+      const cityB = b.textContent.split(",")[0].trim();
+      return cityA.localeCompare(cityB);
+    });
+  }
+
+  if (sortFilter.value === "city-za") {
+    items.sort((a, b) => {
+      const cityA = a.textContent.split(",")[0].trim();
+      const cityB = b.textContent.split(",")[0].trim();
+      return cityB.localeCompare(cityA);
+    });
+  }
+
+   if (sortFilter.value === "country-az") {
+    items.sort((a, b) => {
+      const countryA = a.textContent.split(",")[1].trim();
+      const countryB = b.textContent.split(",")[1].trim();
+      return countryA.localeCompare(countryB);
+    });
+  }
+
+  if (sortFilter.value === "country-az") {
+    items.sort((a, b) => {
+      const countryA = a.textContent.split(",")[1].trim();
+      const countryB = b.textContent.split(",")[1].trim();
+      return countryA.localeCompare(countryB);
+    });
+  }
+
+  locationList.innerHTML = "";
+  items.forEach((item) => locationList.appendChild(item));
+});
+ 
